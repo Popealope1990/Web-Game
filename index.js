8f790c8f665741672fc5ab8aee3924e0daf5ecf7
Benjamin Pope <buddypope1234@gmail.com>
2022-10-01 17:39:01 -0500

    Buttons are now working and cycling pages as they should, building questions and linking them to the questbox next

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 // Grabbing elements for use
 
+// Boxes
+
 const welcomeBox = document.querySelector('.welcomeBox')
-const rulesButton = welcomeBox.querySelector('.bottomBar .rules_button')
-const lbButton = document.getElementsByClassName('.LB_Button')
 const rulesBox = document.querySelector('.rulesBox')
+const questBox = document.querySelector('.questContainer')
+const resultsBox = document.querySelector('.resultsMainContainer')
 
+// buttons
 
-    // Rules button click
+// welcomeBox buttons
+
+const startButton = welcomeBox.querySelector('.start_Button')
+const rulesButton = welcomeBox.querySelector('.bottomBar .rules_button')
+const lbButton1 = welcomeBox.querySelector('.bottomBar .LB_Button')
+
+// rulesBox buttons
+
+const mmButton1 = rulesBox.querySelector('.bottomBar .MM_Button')
+const lbButton2 = rulesBox.querySelector('.bottomBar .LB_Button')
+
+// question box buttons
+
+const mmButton2 = questBox.querySelector('.bottomBar .MM_Button')
+
+// Start button click
+
+startButton.onclick = ()=>{
+    questBox.classList.add('activeInfo')
+    welcomeBox.classList.add('welcomeInact')
+}
+
+// Exit Quiz button
+
+mmButton2.onclick = ()=>{
+    questBox.classList.remove('activeInfo')
+    welcomeBox.classList.remove('welcomeInact')
+}
+
+// Rules button click
+
 rulesButton.onclick = ()=>{
     rulesBox.classList.add('activeInfo')
     welcomeBox.classList.add('welcomeInact')
+}
+
+// rulesBox main menu button click
+
+mmButton1.onclick = ()=>{
+    rulesBox.classList.remove('activeInfo')
+    welcomeBox.classList.remove('welcomeInact')
 }
--- question.js
+++ question.js
@@ -1,1 +1,169 @@
-console.log('Hello from question.js')
+// Making an array of questions
+
+let questions = [
+    {
+        numb: 1,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 11,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 12,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 13,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 14,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+    {
+        numb: 15,
+        question: "What are the strokes of a 4 stroke engine?",
+        answer: "intake, compression, combustion and exhaust",
+        options: [
+            "intake, compression, combustion and ehaust",
+            "injection, rotation, combustion ehaust",
+            "injection, carburetion, rotation and exhaust",
+            "up, down, left, right"
+        ]
+    },
+]
