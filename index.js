d65f4759779cd1e32562ef6d70cdfabb8e3d484e
Benjamin Pope <buddypope1234@gmail.com>
2022-10-01 14:32:27 -0500

    put in final buttons on the last page and started writing the script for the game.  script is slow going

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello from index.js')
+// Grabbing elements for use
+
+const welcomeBox = document.querySelector('.welcomeBox')
+const rulesButton = welcomeBox.querySelector('.bottomBar .rules_button')
+const lbButton = document.getElementsByClassName('.LB_Button')
+const rulesBox = document.querySelector('.rulesBox')
+
+
+    // Rules button click
+rulesButton.onclick = ()=>{
+    rulesBox.classList.add('activeInfo')
+    welcomeBox.classList.add('welcomeInact')
+}
