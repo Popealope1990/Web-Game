b117811080cd53f0e5e1a694de7b5cadd53e7a41
Benjamin Pope <buddypope1234@gmail.com>
2022-10-02 16:52:15 -0500

    writing functions and if else statements to gather my questions, options, and question number and display them as you click

--- index.js
+++ index.js
@@ -7,31 +7,34 @@ const rulesBox = document.querySelector('.rulesBox')
 const questBox = document.querySelector('.questContainer')
 const resultsBox = document.querySelector('.resultsMainContainer')
 
-// buttons
 
-// welcomeBox buttons
+// welcomeBox
 
 const startButton = welcomeBox.querySelector('.start_Button')
 const rulesButton = welcomeBox.querySelector('.bottomBar .rules_button')
 const lbButton1 = welcomeBox.querySelector('.bottomBar .LB_Button')
 
-// rulesBox buttons
+// rulesBox
 
 const mmButton1 = rulesBox.querySelector('.bottomBar .MM_Button')
 const lbButton2 = rulesBox.querySelector('.bottomBar .LB_Button')
 
-// question box buttons
+// question box
 
 const mmButton2 = questBox.querySelector('.bottomBar .MM_Button')
+const contButton = questBox.querySelector('.bottomBar .contButton')
+const questCounter = questBox.querySelector('.questCount')
 
 // Start button click
 
 startButton.onclick = ()=>{
     questBox.classList.add('activeInfo')
     welcomeBox.classList.add('welcomeInact')
+    displayQuestions(0)
+    questionCounter(1)
 }
 
-// Exit Quiz button
+// Exit Quiz button click
 
 mmButton2.onclick = ()=>{
     questBox.classList.remove('activeInfo')
@@ -51,3 +54,43 @@ mmButton1.onclick = ()=>{
     rulesBox.classList.remove('activeInfo')
     welcomeBox.classList.remove('welcomeInact')
 }
+
+// When questBox continue button is clicked
+
+contButton.onclick = ()=>{
+    if(questCount < questions.length - 1){
+        questCount++;
+        questNumber++;
+        displayQuestions(questCount);
+        questionCounter(questNumber);
+    }
+    else{
+        console.log('Questions Completed')
+    }
+}
+
+
+let questCount = 0;
+let questNumber = 1;
+
+// Getting questions and options from array
+
+function displayQuestions(index){
+    const questText = document.querySelector('.question');
+    const optionList = document.querySelector('.optionList');
+    let questTag = '<span>'+ questions[index].question +'</span>';
+    let optTag = '<div class="option">' + questions[index].options[0] + '<span></span></div>'
+                + '<div class="option">' + questions[index].options[1] + '<span></span></div>'
+                + '<div class="option">' + questions[index].options[2] + '<span></span></div>'
+                + '<div class="option">' + questions[index].options[3] + '<span></span></div>';
+    questText.innerHTML = questTag;
+    optionList.innerHTML = optTag;
+}
+
+// Question counter function
+
+function questionCounter(index){
+    let questCountTag = '<span><p>Question</p><p>' + index + '</p><p>of</p><p>' + questions.length + '</p></span>';
+questCounter.innerHTML = questCountTag;
+}
+
--- question.js
+++ question.js
@@ -3,10 +3,10 @@
 let questions = [
     {
         numb: 1,
-        question: "What are the strokes of a 4 stroke engine?",
+        question: "What are the 4 strokes of a 4 stroke engine?",
         answer: "intake, compression, combustion and exhaust",
         options: [
-            "intake, compression, combustion and ehaust",
+            "intake, compression, combustion and exhaust",
             "injection, rotation, combustion ehaust",
             "injection, carburetion, rotation and exhaust",
             "up, down, left, right"
@@ -14,156 +14,156 @@ let questions = [
     },
     {
         numb: 2,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "A crankshafts job in an engine is to?",
+        answer: "change linear motion into rotational motion",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "deliver fuel to the cylinders",
+            "keep cylinder heads in place",
+            "crank start the engine",
+            "change linear motion into rotational motion"
         ]
     },
     {
         numb: 3,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "What are the 2 main components of an engines cooling system?",
+        answer: "radiator and water pump",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "battery and fan",
+            "radiator and water pump",
+            "air intake and turbocharger",
+            "blower motor and condenser"
         ]
     },
     {
         numb: 4,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "What are 3 common ways to deliver fuel to an engine?",
+        answer: "carburetion, port fuel injection, direct fuel injection",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "carburetion, hybridization, electrical charging",
+            "carburetion, port fuel injection, direct fuel injection",
+            "fuel injection, infusion, circulation",
+            "red, green, yellow"
         ]
     },
     {
         numb: 5,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "What are the 3 functions of engine oil?",
+        answer: "lubrication, cooling, cleaning",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "lubrication, making fuel powerful, expell exhaust smell",
+            "cooling, cleaning, making money for Jiffylube",
+            "lubrication, cooling, cleaning",
+            "get dirty, make a mess, spend money "
         ]
     },
     {
         numb: 6,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "Ohm's law does not apply to?",
+        answer: "semi-conductors",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "semi-conductors",
+            "small resistors",
+            "AC currents",
+            "DC currents"
         ]
     },
     {
         numb: 7,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "What is the unit of electrical potential?",
+        answer: "Volts",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "Ampere",
+            "Volts",
+            "Volt-ampere",
+            "Ohms"
         ]
     },
     {
         numb: 8,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "What unit is used to measure resistance?",
+        answer: "Ohms",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "Volts",
+            "Ohms",
+            "Amperes",
+            "Tesla"
         ]
     },
     {
         numb: 9,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "How man cycles does a sine wave go through in 10s at 60hz?",
+        answer: "600 cycles",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "10 cycles",
+            "60 cycles",
+            "120 cycles",
+            "600 cycles"
         ]
     },
     {
         numb: 10,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "What is the safest current value the human body can carry for 3 seconds?",
+        answer: "9 ma",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "4 ma",
+            "9 ma",
+            "15 ma",
+            "25 ma"
         ]
     },
     {
         numb: 11,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "A single acting cylinder can be pressurized:",
+        answer: "from one direction",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "only once",
+            "from one direction",
+            "from both directions",
+            "once after being reset"
         ]
     },
     {
         numb: 12,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "Hydraulic systems use __ to transfer force?",
+        answer: "Liquid",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "Air",
+            "Solid",
+            "Liquid",
+            "Gas"
         ]
     },
     {
         numb: 13,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "Liquids transmit __ in all directions?",
+        answer: "Applied pressure or force",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "Shape",
+            "Work force",
+            "Applied pressure or force",
+            "Light"
         ]
     },
     {
         numb: 14,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "Which of these generates rotary movement using hydraulic power?",
+        answer: "Hydraulic Motor",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "Hydraulic Pump",
+            "Hydraulic Motor",
+            "Pressure Relief Valve",
+            "Cylinder"
         ]
     },
     {
         numb: 15,
-        question: "What are the strokes of a 4 stroke engine?",
-        answer: "intake, compression, combustion and exhaust",
+        question: "What allows flow in one direction only?",
+        answer: "Check Valve",
         options: [
-            "intake, compression, combustion and ehaust",
-            "injection, rotation, combustion ehaust",
-            "injection, carburetion, rotation and exhaust",
-            "up, down, left, right"
+            "Flow Control Valve",
+            "Shut Off Valve",
+            "Pressure Relief Valve",
+            "Check Valve"
         ]
     },
 ]
